c8f25ea40d2a19145ef982a78a8a511f4642afc3
Young AJ Mr (UG - Elec Electronic Eng) <ay00102@surrey.ac.uk>
2018-11-25 12:27:40 +0000

    Add basic publisher/subscriber using std_msgs::String msg type

---
+++ pub_sub/build/gtest/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /usr/src/googletest
+# Build directory: /home/alex/Y3P/catkin_workspace/src/pub_sub/build/gtest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("googlemock")
---
+++ pub_sub/build/gtest/googlemock/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /usr/src/googletest/googlemock
+# Build directory: /home/alex/Y3P/catkin_workspace/src/pub_sub/build/gtest/googlemock
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
