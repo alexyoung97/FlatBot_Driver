23183fdae256c86bce2ae2089e3d5cd77d6c45da
Young AJ Mr (UG - Elec Electronic Eng) <ay00102@surrey.ac.uk>
2018-11-26 09:46:06 +0000

    Talker/listener sending/receiving ModuleStatus msg

--- pub_sub/src/listener.cpp
+++ pub_sub/src/listener.cpp
@@ -1,21 +1,16 @@
 #include "ros/ros.h"
-#include "std_msgs/String.h"
+#include "flat_bot_msgs/ModuleStatus.h"
 
-
-void chatterCallback(const std_msgs::String::ConstPtr& msg)
+void chatterCallback(const flat_bot_msgs::ModuleStatus::ConstPtr& msg)
 {
-  ROS_INFO("I heard: [%s]", msg->data.c_str());
+  ROS_INFO("I heard: [%f]", msg->bus_voltage);
 }
 
 int main(int argc, char **argv)
 {
   ros::init(argc, argv, "listener");
-
   ros::NodeHandle n;
-
   ros::Subscriber sub = n.subscribe("chatter", 1000, chatterCallback);
-
   ros::spin();
-
   return 0;
 }
--- pub_sub/src/talker.cpp
+++ pub_sub/src/talker.cpp
@@ -1,37 +1,26 @@
 #include "ros/ros.h"
-#include "std_msgs/String.h"
+#include "flat_bot_msgs/ModuleStatus.h"
 
 #include <sstream>
 
 int main(int argc, char **argv)
 {
   ros::init(argc, argv, "talker");
-
   ros::NodeHandle n;
-
-  ros::Publisher chatter_pub = n.advertise<std_msgs::String>("chatter", 1000);
-
+  ros::Publisher chatter_pub = n.advertise<flat_bot_msgs::ModuleStatus>("chatter", 1000);
   ros::Rate loop_rate(10);
 
   int count = 0; // count how many messages sent
   while (ros::ok())
   {
-    std_msgs::String msg; // message object
-
-    std::stringstream ss;
-    ss << "hello world " << count;
-    msg.data = ss.str();
-
-    ROS_INFO("%s", msg.data.c_str());
-
+    flat_bot_msgs::ModuleStatus msg; //message object
+    msg.bus_voltage = 5;
+    ROS_INFO("%f", msg.bus_voltage);
     chatter_pub.publish(msg);
-
     ros::spinOnce();
-
     loop_rate.sleep();
     ++count;
   }
-
 
   return 0;
 }
